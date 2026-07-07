2d415d422fcd462c9319d8d4bc89028e55319cf5
sreevasprakash2025cse-art <sreevasprakash.2025.cse@rajalakshmi.edu.in>
2026-07-07 13:17:57 +0530

    Update and rename bmi_calculator.py to grade_calculator.cpp

---
+++ grade_calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+
+    cout << "Enter Marks: ";
+    cin >> marks;
+
+    if(marks >= 90)
+        cout << "Grade A";
+    else if(marks >= 75)
+        cout << "Grade B";
+    else if(marks >= 50)
+        cout << "Grade C";
+    else
+        cout << "Fail";
+
+    return 0;
+}
